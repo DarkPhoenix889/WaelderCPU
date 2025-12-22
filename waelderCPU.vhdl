506f8b322b9f76a65d93ebfc9de327aebc2aedf0
raphaelschmn <166375907+raphaelschmn@users.noreply.github.com>
2025-12-22 15:06:22 +0100

    Update waelderCPU.vhdl
    added common output flags and started working on registers (finished gpr and ir)

--- waelderCPU.vhdl
+++ waelderCPU.vhdl
@@ -36,6 +36,52 @@ end waelderMain;
 
 architecture Behavioral of waelderMain is
 
+    -- flag declaration --
+    ------------------output control flags-------------------------------|
+    signal ctrl_pc_out : in std_logic;      --program counter out
+    signal ctrl_ir_out : in std_logic;      --instruction register out
+    signal ctrl_alu_out : in std_logic;     --arithmetic logical unit out
+    signal ctrl_ram_out : in std_logic;     --random access memory out
+    signal ctrl_ar_out : in std_logic;      --register a out
+    signal ctrl_br_out : in std_logic;      --reg b out
+    signal ctrl_cr_out : in std_logic;      --reg c out
+    signal ctrl_dr_out : in std_logic;      --reg d out
+    signal ctrl_er_out : in std_logic;      --reg e out
+    signal ctrl_lr_out : in std_logic;      --reg l out
+    signal ctrl_hr_out : in std_logic;      --reg h out
+    signal ctrl_mr_out : in std_logic;      --reg m out (16bit)
+
+    -------------------input control flags-------------------------------|
+    signal ctrl _ram_in : in std_logic;     --ram in
+
+
+
+
+    -- register deeclaration --
+    ------------------instruction register-------------------------------|
+    signal i_reg : std_logic_vector (7 downto 0);
+    
+    
+    ----------------------general purpose register-----------------------|
+    signal a_reg : std_logic_vector (7 downto 0);      --reg a
+    signal b_reg : std_logic_vector (7 downto 0);      --reg b
+    signal c_reg : std_logic_vector (7 downto 0);      --reg c
+    signal d_reg : std_logic_vector (7 downto 0);      --reg d
+    signal e_reg : std_logic_vector (7 downto 0);      --reg e
+    signal l_reg : std_logic_vector (7 downto 0);      --reg l
+    signal h_reg : std_logic_vector (7 downto 0);      --reg h
+    signal m_reg : std_logic_vector (15 downto 0);      --reg m (16bit reg - consists out of reg h(-igh) + l(-ow))
+    m_reg(15 downto 8) <= h_reg;    --set highest 8bits of reg m with h reg
+    m_reg(7 downto 0) <= l_reg;     --set lowest 8bits of reg m with l reg
+
+
+process (clk, reset)
+    begin
+        if reset = '1' then
+            -- asynchronous reset - set all flags, registers, etc. to default value (commonly all 0)
+        end if;
+    end process;
+
     --bus
     signal bus : std_logic_vector (7 downto 0);
     bus <= pc when ctrl_pc_out = '1' else
@@ -55,12 +101,7 @@ architecture Behavioral of waelderMain is
 
 
         --program counter
-
-
-
-        --instruction register
-
-
+    
 
         --arithmetic logical unit
 
